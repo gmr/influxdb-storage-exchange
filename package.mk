abee6275570523041f8025b669e4d5c7296a2796
Gavin M. Roy <gavinmroy@gmail.com>
2015-04-21 22:17:09 -0400

    Specify the tests

--- package.mk
+++ package.mk
@@ -1,2 +1,3 @@
-DEPS:=rabbitmq-server rabbitmq-erlang-client ibrowse-wrapper
+DEPS:=rabbitmq-server rabbitmq-erlang-client gun-wrapper
 RELEASABLE:=true
+STANDALONE_TEST_COMMANDS:=eunit:test([influxdb_storage_lib_test,influxdb_storage_parameters_test],[verbose])
